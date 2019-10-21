572aa869126f8e6050d8c06795e0aa1b0f55cd56
Long Tuan Ho <Long.TuanHo@UGent.be>
2019-10-21 19:52:13 +0200

    the third commit

--- c.R
+++ c.R
@@ -1,3 +1,57 @@
 library(abind)
 
 
+library(tidyverse)
+
+library(reshape)
+library(data.table)
+library(xlsx)
+library(gridExtra)
+library(grid)
+library(chron)
+library(devtools)
+library(SOfun)
+
+
+# data visualization 
+
+library(GGally)
+library(RColorBrewer)
+library(proj4)
+library(leaflet)
+library(leaflet.minicharts)
+library(RColorBrewer)
+library(mapview)
+library(htmlwidgets)
+library(corrplot)
+library(mice)
+library(VIM)
+library(ggmosaic)
+library(esquisse)
+
+# data analysis - machine learning - statistics
+
+library(Rcpp)
+library(vegan)
+library(cluster)
+library(MuMIn) # R2c and R2m
+library(nlme)
+library(gamm4)
+library(lme4)
+library(dunn.test)  # Kruskal Wallis test
+library(car)
+library(psych)
+library(psycho)
+library(remotes)
+library(mlr)
+library(mlrMBO)
+library(DiceKriging)
+library(rgenoud)
+library(randomForest)
+library(Metrics)
+library(Hmisc)
+library(xgboost)
+library(checkmate)
+library(ranger)
+library(parallel)
+library(parallelMap)
